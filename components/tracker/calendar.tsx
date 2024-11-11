763318328a4b2ba21c99830901ed0dfea29c0f6b
Kevin Philip <kevinphilip2004@gmail.com>
2024-11-11 10:28:48 -0600

    implemented the calendar Component

--- components/tracker/calendar.tsx
+++ components/tracker/calendar.tsx
@@ -1,29 +1,114 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import {
+  Dimensions,
+  ScrollView,
+  Text,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
+import {
+  addDays,
+  eachDayOfInterval,
+  eachWeekOfInterval,
+  format,
+  subDays,
+} from "date-fns";
+import React, { useMemo, useRef, useState } from "react";
 import Swiper from "react-native-swiper";
 
+const screenWidth = Dimensions.get("window").width;
+
+// Todays date
+const todaysDate = new Date();
+
+// All dates from start to end in a specified interval
+const dates = useMemo(
+  () =>
+    eachWeekOfInterval(
+      // two obj: the interval, which week it starts on
+      {
+        start: subDays(todaysDate, 7),
+        end: addDays(todaysDate, 7),
+      },
+      { weekStartsOn: 1 }
+
+      // your first have an array of each week of an interval
+      // you are reduceing it to get the dates of each day as well
+      // takes to args: callback func & initial value
+      //    - accumulator - 2D array of all weeks,
+      //    - current - cur is just the current date of each week
+    ).reduce((accumulatedDates: Date[][], cur) => {
+      const allDays = eachDayOfInterval({
+        start: cur,
+        end: addDays(cur, 6),
+      });
+
+      accumulatedDates.push(allDays);
+
+      return accumulatedDates;
+    }, []),
+  [todaysDate]
+);
+
 const CalendarWidget = () => {
-  const dates = [
-    { dayname: "Sun", date: new Date() },
-    { dayname: "Sun", date: new Date() },
-    { dayname: "Sun", date: new Date() },
-    { dayname: "Sun", date: new Date() },
-    { dayname: "Sun", date: new Date() },
-    { dayname: "Sun", date: new Date() },
-  ];
+  const [activeDate, setActiveDate] = useState(todaysDate);
+  const scrollViewRef = useRef<ScrollView>(null);
+
+  const handleDateSelected = (
+    day: Date,
+    dayIndex: number,
+    weekIndex: number
+  ) => {
+    setActiveDate(day);
+
+    if (scrollViewRef.current) {
+      const offset =
+        weekIndex * screenWidth + (dayIndex - 1) * (screenWidth / 7);
+      scrollViewRef.current.scrollTo({
+        x: offset > 0 ? offset : 0,
+        animated: true,
+      });
+    }
+  };
 
   return (
-    <View className="bg-green-200 py-6 ">
-      <View className="w-screen flex-row items-start justify-between -mx-4">
-        {dates.map((dateItem, dateIndex) => {
+    <View className="h-32">
+      <ScrollView
+        ref={scrollViewRef}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        pagingEnabled={false} // Allow smooth scrolling instead of snapping
+        className="rounded-xl"
+      >
+        {dates.map((week, weekIndex) => {
           return (
-            <View className="flex-1 h-9 items-center flex-col">
-              <Text>{dateItem.dayname}</Text>
-              <Text>{dateItem.date.getDate()}</Text>
+            <View key={weekIndex} className="bg-app_secondary py-6 flex-row">
+              {week.map((day, dayIndex) => {
+                const dayLetter = format(day, "EEEEE");
+
+                return (
+                  <TouchableWithoutFeedback
+                    key={dayIndex}
+                    onPress={() => handleDateSelected(day, dayIndex, weekIndex)}
+                  >
+                    <View
+                      className={`bg-${
+                        activeDate === day ? "app_secondary" : "app_main"
+                      } flex-col justify-center items-center rounded-lg mx-1 px-6`}
+                    >
+                      <Text className="text-white text-sm w-fit">
+                        {dayLetter}
+                      </Text>
+                      <Text className="text-white text-2xl font-extrabold w-fit">
+                        {day.getDate()}
+                      </Text>
+                    </View>
+                  </TouchableWithoutFeedback>
+                );
+              })}
             </View>
           );
         })}
-      </View>
+      </ScrollView>
     </View>
   );
 };
